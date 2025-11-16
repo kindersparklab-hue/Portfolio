86033fcc14bddd641a3e61bb2ac4d223d3b2f5d5
kindersparklab-hue <kindersparklab@gmail.com>
2025-11-16 16:05:39 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Initialize fullPage.js
+const myFullpage = new fullpage('#fullpage', {
+  autoScrolling: true,
+  navigation: true,
+  navigationPosition: 'right',
+  scrollOverflow: true,
+  sectionsColor: ['#FF6B6B', '#FFD93D', '#6BCB77', '#4D96FF'],
+  anchors: ['about', 'skills', 'projects', 'contact']
+});
